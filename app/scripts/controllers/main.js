6af660f128c86acba49388dea8cf5dcc5bf69bb3
philbot5000 <philbot5000@gmail.com>
2014-06-09 23:02:46 -0600

    Added playlist selector for demo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,17 +2,36 @@
 
 angular.module('ngAudioPlayerApp')
   .controller('MainCtrl', function ($scope, audioPlaylistService) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.myRadPlaylist = [
+        {order: 0, name: 'David Bowie', song: 'John, I\'m only dancing', src: 'audio/bowie.mp3', url: '', twitter: '',},
+        {order: 1, name: 'The Black Angels', song: 'Winter 68', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'},
+        {order: 2, name: 'The Beatles', song: 'Winter 68', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'}
+    ];
+
+    $scope.playlists = [
+      {
+        name: 'Playlist One',
+        list: [
+          {order: 0, name: 'David Bowie', song: 'John, I\'m only dancing', src: 'audio/bowie.mp3', url: '', twitter: '',},
+          {order: 1, name: 'The Black Angels', song: 'Winter 68', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'},
+          {order: 2, name: 'The Beatles', song: 'Winter 68', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'}
+        ]
+      },
+      {
+        name: 'Playlist Two',
+        list: [
+          {order: 0, name: 'Prince', song: 'Raspberry Baret', src: 'audio/bowie.mp3', url: '', twitter: '',},
+          {order: 1, name: 'The Gap Band', song: 'You Dropped a Bomb on Me', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'},
+          {order: 2, name: 'Roy Orbison', song: 'Only the Lonely', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'}
+        ]
+      }
     ];
 
     $scope.playThisList = function() {
       audioPlaylistService.setPlaylist([
-        {order: 0, name: 'Prince', song: 'John, I\'m only dancing', src: 'audio/bowie.mp3', url: '', twitter: '',},
-        {order: 1, name: 'The Gap Band', song: 'Winter 68', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'},
-        {order: 2, name: 'Roy Orbison', song: 'Winter 68', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'}
+        {order: 0, name: 'Prince', song: 'Raspberry Baret', src: 'audio/bowie.mp3', url: '', twitter: '',},
+        {order: 1, name: 'The Gap Band', song: 'You Dropped a Bomb on Me', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'},
+        {order: 2, name: 'Roy Orbison', song: 'Only the Lonely', src: 'http://reviewstalker.com/wp-content/uploads/2011/10/03-Winter-68.mp3'}
       ]);
     };
   });
