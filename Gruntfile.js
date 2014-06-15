319225390a036c2e769c0f482345f0b5abed3c4b
philbot5000 <philbot5000@gmail.com>
2014-06-15 13:38:26 -0600

    rebuild dist for gh-pages

--- Gruntfile.js
+++ Gruntfile.js
@@ -284,7 +284,7 @@ module.exports = function (grunt) {
         }, {
           expand: true,
           dot: true,
-          cwd: 'app/bower_components/font-awesome/fonts/',
+          cwd: '<%= yeoman.app %>/fonts',
           src: ['*.*'],
           dest: '<%= yeoman.dist %>/fonts'
         }]
